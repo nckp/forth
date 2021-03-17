90210e4aa93d48921e1c4696fb66dcb27556e689
Nick P <nick.pius.m@gmail.com>
2021-03-17 18:58:13 +0000

    Add basic parser

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+
+## data
+
+dictionary = {}
+
+## built-in words
+
+
+## parsing and execution
+
+def parse(tok):
+    if tok in dictionary:
+        dictionary[tok]() 
+    elif tok.replace('.', '').replace('-', '').isdigit():
+        try:
+            num = int(tok)
+        except ValueError:
+            num = float(tok)
+    else:
+        raise ValueError("'{0}' is not a valid word or number.".format(tok))
+
+## read tokens from file
+
 def read_chunks(file_object, chunk_size = 1024):
     while True:
         data = file_object.read(chunk_size)
@@ -14,5 +37,14 @@ def read_tokens(file):
 def main():
     file = 'input.forth'
     for tok in read_tokens(file):
-        print(tok)
+        try:
+            parse(tok)
+        except ValueError as e:
+            print(e)
+            return
+
+    print('EOF; done!')
+    
 main()
+
+

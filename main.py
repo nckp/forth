7b82cf6eb032d6ce49ad6bd60ce439e29f819812
Nick P <nick.pius.m@gmail.com>
2021-03-19 19:16:17 +0000

    Add parsing functions

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 
 ## data
+compileMode = False
+
+
+stack = []
 
 dictionary = {}
+
 
 ## built-in words
 
 
-## parsing and execution
+## 
 
 def parse(tok):
     if tok in dictionary:
-        dictionary[tok]() 
-    elif tok.replace('.', '').replace('-', '').isdigit():
+        return dictionary[tok]
+    elif type(tok) is int or type(tok) is float or tok.replace('.', '').replace('-', '').isdigit():
         try:
             num = int(tok)
         except ValueError:
             num = float(tok)
+        return num
     else:
-        raise ValueError("'{0}' is not a valid word or number.".format(tok))
+        raise ValueError("'{0}' is not a valid word or number.".format(tok) )
 
-## read tokens from file
+def compile(tok):
+    pass
+
+def execute(tok):
+    pass
+
+
+## process stream of tokens
 
 def read_chunks(file_object, chunk_size = 1024):
     while True:
@@ -38,7 +51,11 @@ def main():
     file = 'input.forth'
     for tok in read_tokens(file):
         try:
-            parse(tok)
+            result = parse(tok)
+            if compileMode:
+                compile(result)
+            else:
+                execute(result)
         except ValueError as e:
             print(e)
             return
